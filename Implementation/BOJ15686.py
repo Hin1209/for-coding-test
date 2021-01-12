79a584e318aa51ec1d2e29deb100867a83aa7647
Park HyeongIn <hyeongin2024@gmail.com>
2021-01-12 16:53:13 +0900

    Modify code use combinations of itertools

--- Implementation/BOJ15686.py
+++ Implementation/BOJ15686.py
@@ -1,3 +1,4 @@
+from itertools import combinations
 n, m = list(map(int, input().split()))
 city = [list(map(int, input().split())) for _ in range(n)]
 chicken = []
@@ -10,25 +11,22 @@ for i in range(n):
             house.append((i, j))
         elif city[i][j] == 2:
             chicken.append((i, j))
-for i in range(len(chicken)):
-    for j in range(1, len(chicken)):
-        sum_load = 0
-        if(i + j * (m-1) >= len(chicken)):
-            break
-        tmp_chicken = []
-        for k in range(m):
-            tmp_chicken.append(chicken[i+j*k])
-        for z in range(len(house)):
-            min_load = abs(tmp_chicken[0][0] - house[z][0]) + abs(tmp_chicken[0][1] - house[z][1])
-            for l in range(1, len(tmp_chicken)):
-                tmp_load = abs(tmp_chicken[l][0] - house[z][0]) + abs(tmp_chicken[l][1] - house[z][1])
-                if tmp_load < min_load:
-                    min_load = tmp_load
-            sum_load += min_load
-        if f == 0:
+chose_chicken = list(combinations(range(len(chicken)), m))
+for i in chose_chicken:
+    sum_load = 0
+    for y, x in house:
+        min_load = abs(chicken[i[0]][0] - y) + abs(chicken[i[0]][1] - x)
+        for j in range(1, m):
+            tmp = abs(chicken[i[j]][0] - y) + abs(chicken[i[j]][1] - x)
+            if tmp < min_load:
+                min_load = tmp
+        sum_load += min_load
+    if f == 0:
+        f += 1
+        min_chicken_load = sum_load
+    else:
+        if sum_load < min_chicken_load:
             min_chicken_load = sum_load
-            f += 1
-        else:
-            if min_chicken_load > sum_load:
-                min_chicken_load = sum_load
+
+
 print(min_chicken_load)
