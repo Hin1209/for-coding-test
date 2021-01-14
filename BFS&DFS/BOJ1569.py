f7e64094e787844e07bfd5d9555b9eb68a442599
Park HyeongIn <hyeongin2024@gmail.com>
2021-01-14 17:05:53 +0900

    Revise BOJ1569.py for reducing time

--- BFS&DFS/BOJ1569.py
+++ BFS&DFS/BOJ1569.py
@@ -2,7 +2,6 @@ import sys
 from collections import deque
 
 m, n, h = list(map(int, sys.stdin.readline().split()))
-direction = [(0, 1, 0), (1, 0, 0), (0, 0, 1), (-1, 0, 0), (0, -1, 0), (0, 0, -1)]
 box = [[[0 for _ in range(m)] for _ in range(n)] for _ in range(h)]
 tmp_box = []
 check_zero = 0
@@ -21,19 +20,60 @@ for z in range(h):
             elif box[z][y][x] == 0:
                 check_zero += 1
 time = 0
+execpt = 0
+if check_zero == 0:
+    execpt = 1
 while q:
+    if execpt == 1:
+        break
     z, y, x = q.popleft()
-    for dz, dy, dx in direction:
-        nz, ny, nx = z + dz, y + dy, x + dx
-        if 0 <= nz < h and 0 <= ny < n and 0 <= nx < m:
-            if visited[nz][ny][nx] == 0 and box[nz][ny][nx] == 0:
-                visited[nz][ny][nx] = visited[z][y][x]+1
-                time = max(time, visited[nz][ny][nx])
-                box[nz][ny][nx] = 1
-                q.append([nz, ny, nx])
-                check_zero -= 1
+    if z > 0:
+        if visited[z-1][y][x] == 0 and box[z-1][y][x] == 0:
+            visited[z-1][y][x] = visited[z][y][x] + 1
+            time = max(time, visited[z-1][y][x])
+            box[z-1][y][x] = 1
+            q.append([z-1, y, x])
+            check_zero -= 1
+    if z < h-1:
+        if visited[z+1][y][x] == 0 and box[z+1][y][x] == 0:
+            visited[z+1][y][x] = visited[z][y][x] + 1
+            time = max(time, visited[z+1][y][x])
+            box[z+1][y][x] = 1
+            q.append([z+1, y, x])
+            check_zero -= 1
+    if y > 0:
+        if visited[z][y-1][x] == 0 and box[z][y-1][x] == 0:
+            visited[z][y-1][x] = visited[z][y][x] + 1
+            time = max(time, visited[z][y-1][x])
+            box[z][y-1][x] = 1
+            q.append([z, y-1, x])
+            check_zero -= 1
+    if y < n-1:
+        if visited[z][y+1][x] == 0 and box[z][y+1][x] == 0:
+            visited[z][y+1][x] = visited[z][y][x] + 1
+            time = max(time, visited[z][y+1][x])
+            box[z][y+1][x] = 1
+            q.append([z, y+1, x])
+            check_zero -= 1
+    if x > 0:
+        if visited[z][y][x-1] == 0 and box[z][y][x-1] == 0:
+            visited[z][y][x-1] = visited[z][y][x] + 1
+            time = max(time, visited[z][y][x-1])
+            box[z][y][x-1] = 1
+            q.append([z, y, x-1])
+            check_zero -= 1
+    if x < m-1:
+        if visited[z][y][x+1] == 0 and box[z][y][x+1] == 0:
+            visited[z][y][x+1] = visited[z][y][x] + 1
+            time = max(time, visited[z][y][x+1])
+            box[z][y][x+1] = 1
+            q.append([z, y, x+1])
+            check_zero -= 1
 
-if check_zero:
-    print(-1)
+if execpt == 1:
+    print(0)
 else:
-    print(time-1)
+    if check_zero != 0:
+        print(-1)
+    else:
+        print(time-1)
