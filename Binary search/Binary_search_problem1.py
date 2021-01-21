5b0530c3fa26ac17a7f97c09cd9b399da2ed84bb
Park HyeongIn <hyeongin2024@gmail.com>
2021-01-21 14:44:08 +0900

    Update Binary_search_problem1.py

--- Binary search/Binary_search_problem1.py
+++ Binary search/Binary_search_problem1.py
@@ -1,39 +1,35 @@
 import sys
+
+# 왼쪽 끝 인덱스를 찾는 함수
+def search_left(array, start, end, target):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if (mid == 0 or array[mid-1] < target) and array[mid] == target:
+        return mid
+    elif array[mid] < target:
+        return search_left(array, mid + 1, end, target)
+    elif array[mid] > target:
+        return search_left(array, start, mid - 1, target)
+
+# 오른쪽 끝 인덱스를 찾는 함수
+def search_right(array, start, end, target):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if (mid == len(array)-1 or array[mid + 1] > target) and array[mid] == target:
+        return mid
+    elif array[mid] < target:
+        return search_right(array, mid + 1, end, target)
+    elif array[mid] > target:
+        return search_right(array, start, mid - 1, target)
+
+
 n, x = list(map(int, sys.stdin.readline().split()))
 data = list(map(int, sys.stdin.readline().split()))
 start = 0
 end = len(data) - 1
-left = 0
-right = 0
-left_stop = 0
-right_stop = 0
-while True:
-    mid = (start + end) // 2
-    if data[mid] == x:
-        if mid == 0:
-            left = mid
-            left_stop = 0
-        if mid == len(data) - 1:
-            right = mid
-            right_stop = 0
-        if mid > 0 and data[mid-1] == x and left_stop == 0:
-            end = mid - 1
-            continue
-        elif mid > 0 and data[mid-1] < x and left_stop == 0:
-            left = mid
-            left_stop = 1
-            continue
-        if mid < len(data) - 1 and data[mid+1] == x and right_stop == 0:
-            start = mid + 1
-            continue
-        elif mid < len(data) - 1 and data[mid+1] > x and right_stop == 0:
-            right = mid
-            right_stop = 1
-            continue
-    elif data[mid] < x:
-        start = mid + 1
-    elif data[mid] > x:
-        end = mid - 1
-    if right_stop == 1 and left_stop == 1:
-        break
+left = search_left(data, start, end, x)
+right = search_right(data, start, end, x)
+
 print(right - left + 1)
