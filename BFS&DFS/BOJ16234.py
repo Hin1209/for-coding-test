3322af90b5afb2b1f31300b95fb71a0c13298f80
Park HyeongIn <hyeongin2024@gmail.com>
2021-01-21 14:37:25 +0900

    Update BOJ16234.py

--- BFS&DFS/BOJ16234.py
+++ BFS&DFS/BOJ16234.py
@@ -1,112 +1,61 @@
 from collections import deque
+import sys
+n, l, r = list(map(int, sys.stdin.readline().rstrip().split()))
+country = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
 
-n, l, r = list(map(int, input().split()))
-country = [list(map(int, input().split())) for _ in range(n)]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
-q = deque()
-relieve = [[0 for _ in range(n)] for _ in range(n)]
+# 인구가 이동한 횟수
 circular = 0
+
 while True:
     if n == 1:
         break
-    cnt = 0
-    q.append([0, 0])
-    visited = [[0 for _ in range(n)] for _ in range(n)]
-    visited[0][0] = 1
-    while q:
-        y, x = q.popleft()
-        if y > 0:
-            if l <= abs(country[y][x] - country[y-1][x]) <= r:
-                if relieve[y][x] == 0 and relieve[y-1][x] == 0:
-                    cnt += 1
-                    relieve[y][x] = cnt
-                if relieve[y][x] != relieve[y-1][x] and relieve[y][x] != 0 and relieve[y-1][x] != 0:
-                    relieve[y-1][x] = min(relieve[y][x], relieve[y-1][x])
-                    relieve[y][x] = min(relieve[y][x], relieve[y-1][x])
-                    cnt = min(relieve[y][x], relieve[y-1][x])
-            if visited[y-1][x] == 0:
-                q.append([y-1, x])
-                visited[y-1][x] = 1
-        if y < n-1:
-            if l <= abs(country[y][x] - country[y+1][x]) <= r:
-                if relieve[y][x] == 0 and relieve[y + 1][x] == 0:
-                    cnt += 1
-                    relieve[y][x] = cnt
-                if relieve[y][x] != relieve[y+1][x] and relieve[y][x] != 0 and relieve[y+1][x] != 0:
-                    relieve[y+1][x] = min(relieve[y][x], relieve[y+1][x])
-                    relieve[y][x] = min(relieve[y][x], relieve[y+1][x])
-                    cnt = min(relieve[y][x], relieve[y+1][x])
-            if visited[y+1][x] == 0:
-                q.append([y+1, x])
-                visited[y+1][x] = 1
-        if x > 0:
-            if l <= abs(country[y][x] - country[y][x-1]) <= r:
-                if relieve[y][x] == 0 and relieve[y][x-1] == 0:
-                    cnt += 1
-                    relieve[y][x] = cnt
-                if relieve[y][x] != relieve[y][x-1] and relieve[y][x] != 0 and relieve[y][x-1] != 0:
-                    relieve[y-1][x] = min(relieve[y][x], relieve[y-1][x])
-                    relieve[y][x] = min(relieve[y][x], relieve[y-1][x])
-                    cnt = min(relieve[y][x], relieve[y][x-1])
-            if visited[y][x-1] == 0:
-                q.append([y, x-1])
-                visited[y][x-1] = 1
-        if x < n-1:
-            if l <= abs(country[y][x] - country[y][x+1]) <= r:
-                if relieve[y][x] == 0 and relieve[y][x+1] == 0:
-                    cnt += 1
-                    relieve[y][x] = cnt
-                if relieve[y][x] != relieve[y][x+1] and relieve[y][x] != 0 and relieve[y][x+1] != 0:
-                    relieve[y][x+1] = min(relieve[y][x], relieve[y][x+1])
-                    relieve[y][x] = min(relieve[y][x], relieve[y][x+1])
-                    cnt = min(relieve[y][x], relieve[y][x+1])
-            if visited[y][x+1] == 0:
-                q.append([y, x+1])
-                visited[y][x+1] = 1
-    vi = deque()
-    stop = 0
-    if cnt == 0:
+    # 동맹을 나타내기 위한 리스트
+    alliance = [[0 for _ in range(n)] for _ in range(n)]
+    # 연합 번호
+    cnt = 1
+    for i in range(n):
+        for j in range(n):
+            # 동맹이 없을 경우 주변에 동맹을 맺을 수 있는 국가가 있는지 검사
+            if alliance[i][j] == 0:
+                q = deque()
+                q.append([i, j])
+                # 동맹 국가의 총 인원
+                sum_people = country[i][j]
+                # 연합 국가의 수
+                union = 1
+                # 연합 국가의 좌표
+                ally = []
+                ally.append([i, j])
+                alliance[i][j] = cnt
+                while q:
+                    y, x = q.popleft()
+                    for c in range(4):
+                        ny = y + dy[c]
+                        nx = x + dx[c]
+                        if 0 <= nx < n and 0 <= ny < n:
+                            # 동맹을 맺을 수 있는 국가면 동맹에 추가시킴
+                            if l <= abs(country[y][x] - country[ny][nx]) <= r and alliance[ny][nx] == 0:
+                                alliance[ny][nx] = cnt
+                                union += 1
+                                sum_people += country[ny][nx]
+                                q.append([ny, nx])
+                                ally.append([ny, nx])
+                # 동맹 국가의 인원을 나눔
+                for ay, ax in ally:
+                    country[ay][ax] = sum_people // union
+                cnt += 1
+    # 더 이상 인구 이동이 없는 경우 반복문 종료
+    if cnt == n * n + 1:
         break
-    for i in range(1, cnt+1):
-        for ii in range(n):
-            for jj in range(n):
-                if relieve[ii][jj] == i:
-                    vi.append([ii, jj])
-                    link = set()
-                    link.add((ii, jj))
-                    result = 0
-                    relieve[ii][jj] = 0
-                    while vi:
-                        vy, vx = vi.popleft()
-                        if vy > 0:
-                            if relieve[vy-1][vx] == i:
-                                vi.append([vy-1, vx])
-                                relieve[vy-1][vx] = 0
-                                link.add((vy-1, vx))
-                        if vy < n-1:
-                            if relieve[vy+1][vx] == i:
-                                vi.append([vy+1, vx])
-                                relieve[vy+1][vx] = 0
-                                link.add((vy+1, vx))
-                        if vx > 0:
-                            if relieve[vy][vx-1] == i:
-                                vi.append([vy, vx-1])
-                                relieve[vy][vx-1] = 0
-                                link.add((vy, vx-1))
-                        if vx < n-1:
-                            if relieve[vy][vx+1] == i:
-                                vi.append([vy, vx+1])
-                                relieve[vy][vx+1] = 0
-                                link.add((vy, vx+1))
-                    for ky, kx in link:
-                        result += country[ky][kx]
-                    result = int(result/len(link))
-                    for cy, cx in link:
-                        country[cy][cx] = result
-                else:
-                    stop += 1
     circular += 1
 print(circular)
+
+
+
+
 
 
 # 5 10 20
