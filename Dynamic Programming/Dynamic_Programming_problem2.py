8431db3006e4c4afdc1ca523c3e059ff1ef5e758
Park HyeongIn <hyeongin2024@gmail.com>
2021-01-22 17:07:59 +0900

    Update Dynamic_Programming_problem2.py

--- Dynamic Programming/Dynamic_Programming_problem2.py
+++ Dynamic Programming/Dynamic_Programming_problem2.py
@@ -1,22 +1,22 @@
 n = int(input())
-dp_table = [0 for _ in range(100000001)]
-dp_table[1] = 1
-cnt = 0
-for i in range(len(dp_table)):
-    if dp_table[i] == 1:
-        if i * 2 >= len(dp_table):
-            break
-        dp_table[i * 2] = 1
-        if i * 3 >= len(dp_table):
-            continue
-        dp_table[i * 3] = 1
-        if i * 5 >= len(dp_table):
-            continue
-        dp_table[i * 5] = 1
-for i in range(1, len(dp_table)):
-    if dp_table[i] == 1:
-        cnt += 1
-    if cnt == n:
-        print(i)
-        break
+dp_table = [0] * n
+dp_table[0] = 1
+# 2배, 3배, 5배를 위한 인덱스
+i2 = i3 = i5 = 0
+# 처음 곱셈 해줄 값을 초기화 시킴
+next2, next3, next5 = 2, 3, 5
+
+for i in range(1, n):
+    dp_table[i] = min(next2, next3, next5)
+
+    if dp_table[i] == next2:
+        i2 += 1
+        next2 = dp_table[i2] * 2
+    if dp_table[i] == next3:
+        i3 += 1
+        next3 = dp_table[i3] * 3
+    if dp_table[i] == next5:
+        i5 += 1
+        next5 = dp_table[i5] * 5
+print(dp_table[n-1])
 
